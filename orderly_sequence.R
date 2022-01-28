bb17645ada0437c8b53a8d415fdde8fda3ae7838
paddy7wb <paddy7wb@gmail.com>
2022-01-28 16:54:25 +0000

    Orderly sequence performs all aggregations and plots

--- orderly_sequence.R
+++ orderly_sequence.R
@@ -1,3 +1,5 @@
+#### Initial Orderly Repo Setup ####
+
 # initialise orderly repo
 # orderly::orderly_init(root = "~/imperial_repos/circumcision-coverage-orderly/")
 
@@ -8,12 +10,33 @@
 # orderly::orderly_use_gitignore(prompt = FALSE)
 
 # create a directory in src from which to run modelling for LSO
-# orderly::orderly_new("01_LSO_model")
+# orderly::orderly_new("01_modelling")
 
 # fill in ordelry.yml with artefacts, parameters, global resources, etc
 # write out script to be run
-# run script!
-orderly::orderly_run("01_LSO_model", list(cntry = "LSO"))
+
+#### Run src Scripts ####
+
+# set country parameter
+iso3 <- "LSO"
+
+# take directories/orderly tasks in src
+dirs <- list.dirs(path = "src", full.names = FALSE)
+
+## modelling
+orderly::orderly_run("01_modelling", list(cntry = iso3))
+
+## aggregations
+# aggregation orderly tasks
+aggregations <- dirs[dirs %like% "02"]
+lapply(aggregations, orderly::orderly_run, list(cntry = iso3))
+
+## plots
+# plotting orderly tasks
+plots <- dirs[dirs %like% "03"]
+lapply(plots, orderly::orderly_run, list(cntry = iso3))
+
+#### Committing Reports #### 
 
 # list draft reports
 (dr <- orderly::orderly_list_drafts())
