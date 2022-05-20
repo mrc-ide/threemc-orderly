7ac06c81694baee48a26a139b6f223219b1c50e9
paddy7wb <paddy7wb@gmail.com>
2022-05-20 13:05:21 +0100

    replace modelling with shorter script using new funs

--- src/01_modelling/script.R
+++ src/01_modelling/script.R
@@ -21,29 +21,23 @@ rm_missing_type <- FALSE
 
 # read in data, filter for specific country and male surveys only
 filters <- c("iso3" = cntry, sex = "male")
-areas <- read_circ_data(
-    "areas.geojson",
-    filters
-)
-areas <- st_make_valid(areas) # move this to function
+areas <- read_circ_data("areas.geojson", filters) %>% 
+  dplyr::mutate(space = 1:dplyr::n()) # add space column to areas
+areas <- st_make_valid(areas) 
+
 survey_clusters <- read_circ_data("survey_clusters.csv.gz", filters)
 survey_individuals <- read_circ_data("survey_individuals.csv.gz", filters)
-survey_circumcision <- read_circ_data(
-  "survey_circumcision.csv.gz", 
-  filters
-)
+survey_circumcision <- read_circ_data("survey_circumcision.csv.gz", filters)
 # also read in population data
-populations <- read_circ_data(
-  here::here("population_singleage.csv.gz"),
-  filters
-)
-
+populations <- read_circ_data("population_singleage_aggr.csv.gz", filters)
 
 # pull recommended area hierarchy for target country
 area_lev <- threemc::datapack_psnu_area_level %>%
   filter(iso3 == cntry) %>%
   pull(psnu_area_level)
-if (area_lev == 0) area_lev <- NULL # don't model at the country level
+
+# don't model at the country level
+if (length(area_lev) > 0 && area_lev == 0) area_lev <- NULL 
 
 # if area_level is missing, assume most common area lev in surveys
 if (length(area_lev) == 0) {
@@ -51,9 +45,7 @@ if (length(area_lev) == 0) {
   area_lev <- as.numeric(names(area_lev)[area_lev == max(area_lev)])
 }
 
-#####################################
 #### Preparing circumcision data ####
-#####################################
 # pull latest census year from survey_id
 cens_year <- max(as.numeric(
   substr(unique(survey_circumcision$survey_id), 4, 7)
@@ -74,7 +66,7 @@ survey_circumcision <- prepare_survey_data(
 )
 
 if (nrow(survey_circumcision) == 0) {
-  message("no valid surveys at this level")
+  message("no valid surveys at this level") # move inside function!
 }
 
 # include indicator to determine whether there is any type distinction for cntry
@@ -85,12 +77,7 @@ if (all(is.na(survey_circumcision$circ_who) &
 } else is_type <- TRUE
 
 
-# rm unnecessary datasets
-rm(survey_clusters, survey_individuals); gc()
-
-################################################
-### Shell dataset to estimate empirical rate ###
-################################################
+#### Shell dataset to estimate empirical rate ####
 
 # Skeleton dataset
 
@@ -125,72 +112,19 @@ out <- create_shell_dataset(
   circ                = "indweight_st"
 )
 
-# remove unnecessary datasets
-rm(survey_circumcision); gc()
+#### Dataset for modelling ####
 
-#############################
-### Dataset for modelling ###
-#############################
+dat_tmb <- threemc_prepare_model_data(
+  out        = out,
+  areas      = areas,
+  area_lev   = area_lev,
+  aggregated = TRUE,
+  weight     = "population",
+  k_dt       = k_dt
+)
 
-# Create design matrices for fixed effects and temporal, age, space and
-# interaction random effects
-#
-# Code has been updated to only produce the design matrices on the lowest
-# area_level as the shell dataset contains multiple area_levels now.
-design_matrices <- create_design_matrices(dat = out,
-                                          area_lev = area_lev,
-                                          k_dt = k_dt)
 
-# Create integration matrices for selecting the instantaneous hazard rate
-#
-# Code has been updated to produce the integration matrices on the lowest
-# area_level as the shell dataset contains multiple area_levels now.
-#
-# Updated both integration matrices functions so that the two cases where
-# we have and don't have a stratification variable are dealt with the same
-# piece of code. Done by introducing a dummy variable when no stratification
-# variables specified.
-integration_matrices <- create_integration_matrices(out,
-                                                    area_lev = area_lev,
-                                                    time1 = "time1",
-                                                    time2 = "time2",
-                                                    age = "age",
-                                                    strat = "space")
-
-# The function was first updated so that the two cases where we have and don't
-# have a stratification variable are dealt with the same piece of code. Done
-# by introducing a dummy variable when no stratification variables specified.
-#
-# The secondly the function now considers two cases when we want to run (a)
-# on all areas and (b) aggregated from the reference area level. The former
-# is needed to calculate the PYRS and the latter is needed for the likelihood
-# of the function.
-# create survival matrices for MMC, TMC, censored and left censored
-survival_matrices <- create_survival_matrices(out,
-                                              areas = areas,
-                                              area_lev = area_lev,
-                                              time1 = "time1",
-                                              time2 = "time2",
-                                              age = "age",
-                                              strat = "space",
-                                              aggregated = TRUE,
-                                              weight = 'population')
-
-# Precision/Adjacency matrix for the spatial random effects
-Q_space <- list("Q_space" =
-                  create_icar_prec_matrix(sf_obj    = areas,
-                                          area_lev = area_lev,
-                                          row.names = "space"))
-
-# Combine Data for tmb model
-# (could do this by looping through `create_matrices` functions!)
-dat_tmb <- c(design_matrices, survival_matrices, integration_matrices, Q_space)
-
-rm(design_matrices, survival_matrices, integration_matrices, Q_space); gc()
-
-###########################################
-### Modelling circumcision probabilites ###
-###########################################
+#### Modelling circumcision probabilites ####
 # specify TMB model
 if (is_type == TRUE) {
   mod <- "Surv_SpaceAgeTime_ByType_withUnknownType"
@@ -201,7 +135,7 @@ parameters <- with(
   dat_tmb,
   list(
     # intercept
-    "u_fixed_mmc"          = rep(-5, ncol(X_fixed_mmc)),
+    "u_fixed_mmc"            = rep(-5, ncol(X_fixed_mmc)),
     "u_fixed_tmc"            = rep(-5, ncol(X_fixed_tmc)),
     # age random effect
     "u_age_mmc"              = rep(0, ncol(X_age_mmc)),
@@ -235,76 +169,29 @@ parameters <- with(
     "logitrho_mmc_age1"      = 2,
     "logitrho_mmc_age2"      = 2,
     "logitrho_mmc_age3"      = 2,
-    "logitrho_tmc_age1"     = 2,
+    "logitrho_tmc_age1"      = 2,
     "logitrho_tmc_age2"      = 2
   )
 )
 
-# random effects
-randoms <- c("u_time_mmc", "u_age_mmc", "u_space_mmc",
-             "u_agetime_mmc", "u_agespace_mmc", "u_spacetime_mmc",
-             "u_age_tmc", "u_space_tmc", "u_agespace_tmc")
-
-if (is_type == FALSE) {
-  
-  remove_type_distinction <- function(x) {
-    names(x) <- stringr::str_remove(names(x), "_mmc")
-    x <- x[!names(x) %like% "_tmc"]
-  }
-  
-  dat_tmb <- remove_type_distinction(
-    dat_tmb[!names(dat_tmb) %in% c("A_mmc", "A_tmc")]
-  )
-  names(dat_tmb)[names(dat_tmb) == "A_mc"] <- "A"
-  
-  parameters <- remove_type_distinction(parameters)
-  
-  randoms <- stringr::str_remove(randoms, "_mmc")
-  randoms <- randoms[!randoms %like% "_tmc"]
-  # randoms <- randoms[!randoms %in% c("u_space"     "u_agespace"  "u_spacetime")]
-}
-
-# ?
-randoms <- randoms[randoms %in% names(parameters)]
-if (length(randoms) == 0) {
-  randoms <- NULL
-}
-
-# Create TMB object
-obj <- TMB::MakeADFun(dat_tmb,
-                      parameters,
-                      random = randoms,
-                      method = "BFGS",
-                      hessian = TRUE,
-                      DLL = mod)
-
-rm(dat_tmb, parameters); gc()
-
-# Run optimiser
-opt <- stats::nlminb(
-  start   = obj$par,
-  obj     = obj$fn,
-  gr      = obj$gr,
-  control = list(trace = 1)
+fit <- threemc_fit_model(
+  dat_tmb    = dat_tmb,
+  mod        = mod,
+  parameters = parameters,
+  randoms    = c("u_time_mmc", "u_age_mmc", "u_space_mmc",
+                 "u_agetime_mmc", "u_agespace_mmc", "u_spacetime_mmc",
+                 "u_age_tmc", "u_space_tmc", "u_agespace_tmc"),
+  N = N
 )
 
-# sample from TMB fit
-fit <- circ_sample_tmb(obj, opt, nsample = N)
+# subset to specific area level and calculate quantiles for rates and hazard
+out_spec <- compute_quantiles(out, fit, area_lev = area_lev)
 
-rm(obj, opt); gc()
 
-# subset to specific are level
-out_spec <- out %>%
-  filter(area_level == area_lev)
+#### saving results ####
 
-# calculate quantiles for rates and cumulative hazard
-out_spec <- compute_quantiles(out_spec, fit)
-
-######################
-### saving results ###
-######################
-# prepare for output (could surely put this in function and make more terse!)
-out_spec <- out_spec %>% # no obs_mc?
+# prepare for output
+out_spec <- out_spec %>%
   select(
     area_id, area_name, area_level, year, age = circ_age,
     contains("obs"),
@@ -315,17 +202,19 @@ out_spec <- out_spec %>% # no obs_mc?
     contains("inc_mmc"), contains("inc_tmc"), contains("inc")
   )
 
-# Saving results (also make into function)
-data.table::fwrite(out, file = "Results_DistrictAgeTime_ByType_full.csv.gz")
-data.table::fwrite(out_spec, file = "Results_DistrictAgeTime_ByType.csv.gz")
+# minimise fit object for saving
+fit <- minimise_fit_obj(fit, dat_tmb, parameters)
 
-# save smaller TMB object
-fit_small <- fit
-fit_small$tmb_data <- dat_tmb
-fit_small$par_init <- parameters
-fit_small$sample <- NULL
-fit_small$obj <- NULL
-saveRDS(fit_small, "TMBObjects_DistrictAgeTime_ByType.rds")
+# Saving results (also make into function)
+data.table::fwrite(
+  out_spec,
+  file = here::here(
+    paste0(save_loc, cntry, "_Results_DistrictAgeTime_ByType.csv.gz")
+  )
+)
+
+# save fit as .rds file
+saveRDS(fit, "TMBObjects_DistrictAgeTime_ByType.rds")
 
 # Plotting results 
 # Coverage
