07fed945dac70368d12fe610cff68a1a08fff2a5
paddy7wb <paddy7wb@gmail.com>
2022-07-01 12:06:10 +0100

    add comparisons plots to App

--- Shiny/App.R
+++ Shiny/App.R
@@ -34,14 +34,18 @@ source(
   paste0(orderly_root, "/Shiny/modules/02_ssa_plots/ssa_plots_module.R")
 )
 
+source(
+  paste0(orderly_root, "/Shiny/modules/03_comparison_plots/comparison_plots_module.R")
+)
+
 #### Data ####
 
-ssa_iso3 <- c(
+ssa_iso3 <- comparison_iso3 <- sort(c(
   "AGO", "BDI", "BEN", "BFA", "BWA", "CAF", "CIV", "CMR", "COD",
   "COG", "ETH", "GAB", "GHA", "GIN", "GMB", "GNB", "GNQ", "KEN",
   "LBR", "LSO", "MLI", "MOZ", "MWI", "NAM", "NER", "NGA", "RWA",
   "SEN", "SLE", "SWZ", "TCD", "TGO", "TZA", "UGA", "ZAF", "ZMB", "ZWE"
-)
+))
 
 # shapefile
 areas_loc <- orderly::orderly_list_archive() %>% 
@@ -55,16 +59,57 @@ areas_loc <- file.path(
   "artefacts/areas.geojson"
 )
 areas <- read_circ_data(areas_loc) %>% 
-  filter(iso3 %in% ssa_iso3) %>% 
+  filter(iso3 %in% ssa_iso3) 
+
+areas_join <- areas %>%
+  sf::st_drop_geometry() %>%
+  dplyr::select(
+    iso3,       area_id,          area_name,
+    area_level, area_level_label, area_sort_order
+  ) %>%
+  distinct()
+
+areas <- areas %>% 
   group_by(area_level) %>%
   dplyr::mutate(space = row_number()) %>%
   ungroup()
 
+# DMPPT2 data
+dmppt2_data <- readr::read_csv(
+  paste0(orderly_root, "/Shiny/global/dmppt2-2021_circumcision_coverage.csv.gz")
+)
+
+# only use iso3s with DMPPT2 data (?)
+comparison_iso3 <- comparison_iso3[comparison_iso3 %in% dmppt2_data$iso3]
+
+# survey_data
+survey_data <- read_circ_data(
+  paste0(orderly_root, "/Shiny/global/survey-circumcision-coverage.csv.gz"),
+  filters = c("sex" = "male")
+) %>% 
+  filter(iso3 %in% comparison_iso3)
+
+# change naming convention of survey data (not working currently!)
+if ("survey_mid_calendar_quarter" %in% names(survey_data)) {
+  survey_data <- survey_data %>%
+    rename(
+      year = survey_mid_calendar_quarter,
+      mean = estimate,
+      sd = std_error,
+      lower = ci_lower,
+      upper = ci_upper
+    )
+}
+
 # data which is fed into Shiny app
 all_plots_data <- list(
-  "ssa_iso3" = ssa_iso3,
-  "orderly_root" = orderly_root,
-  "areas"    = areas
+  "orderly_root"    = orderly_root,
+  "ssa_iso3"        = ssa_iso3,
+  "areas"           = areas,
+  "areas_join"      = areas_join,
+  "comparison_iso3" = comparison_iso3,
+  "dmppt2_data"     = dmppt2_data,
+  "survey_data"     = survey_data
 )
 
 #### UI #####
@@ -95,6 +140,12 @@ ui = fluidPage(
           br(),
           br(),
           ssa_plots_UI("ssa_plots1"),
+        ),
+        tabPanel(
+          "Comparisons",
+          br(),
+          br(),
+          comparison_plots_UI("comparison_plots1"),
         )
       )
     )
@@ -110,15 +161,21 @@ server <- function(input, output, session) {
   callModule(
     single_plots_server,
     "single_plots1",
-    data = all_plots_data
+    data = all_plots_data,
+    selected = reactive("LSO")
   )
   callModule(
     ssa_plots_server,
     "ssa_plots1",
     data = all_plots_data
   )
-  # callModule(supplyServer,'supply1',connection=con,selected=reactive('United Kingdom'),
-  #            data=reactive(list("hesitancy_df" = hesitancy_df,"owid" = owid)))
-  
+  callModule(
+    comparison_plots_server,
+    "comparison_plots1",
+    data = all_plots_data,
+    selected = reactive("LSO")
+  )
 }
+
+#### Run App #### 
 shinyApp(ui = ui, server = server)
