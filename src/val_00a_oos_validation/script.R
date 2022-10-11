00e43748591b637b43dafccde99fde4940502fcc
paddy7wb <paddy7wb@gmail.com>
2022-10-11 10:20:43 +0100

    Add optional area_lev parameter to val_00a_oos_validation task

--- src/val_00a_oos_validation/script.R
+++ src/val_00a_oos_validation/script.R
@@ -24,6 +24,7 @@ if (cntry == "LBR") cens_age <- 29 else cens_age <- 59
 forecast_year <- 2021
 paed_age_cutoff <- 10
 
+
 #### Reading in data ####
 
 # Revert to using planar rather than spherical geometry in `sf`
@@ -41,20 +42,22 @@ populations <- read_circ_data(
   filters
 )
 
-# pull recommended area hierarchy for target country
-area_lev <- threemc::datapack_psnu_area_level %>%
-  filter(iso3 == cntry) %>%
-  pull(psnu_area_level)
-
-# don't model at the country level
-if (length(area_lev) > 0 && area_lev == 0) area_lev <- NULL
-
-# if area_level is missing, assume most common area lev in surveys
-if (length(area_lev) == 0) {
-  area_lev <- table(as.numeric(substr(survey_circumcision$area_id, 5, 5)))
-  area_lev <- as.numeric(names(area_lev)[area_lev == max(area_lev)])
+if (is.null(area_lev)) {
+  # pull recommended area hierarchy for target country
+  area_lev <- threemc::datapack_psnu_area_level %>%
+    filter(iso3 == cntry) %>%
+    pull(psnu_area_level)
+  
+  # don't model at the country level
+  if (length(area_lev) > 0 && area_lev == 0) area_lev <- NULL
+  
+  # if area_level is missing, assume most common area lev in surveys
+  if (length(area_lev) == 0) {
+    area_lev <- table(as.numeric(substr(survey_circumcision$area_id, 5, 5)))
+    area_lev <- as.numeric(names(area_lev)[area_lev == max(area_lev)])
+  }
 }
-
+  
 
 #### remove most recent survey #### 
 
