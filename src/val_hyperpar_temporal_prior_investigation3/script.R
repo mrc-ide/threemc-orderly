51c59eba84b94679bf9470c1e6d5e444fd7ab7dc
= <paddy7wb@gmail.com>
2023-03-30 11:15:44 +0000

    Don't check rw_order twice

--- src/val_hyperpar_temporal_prior_investigation3/script.R
+++ src/val_hyperpar_temporal_prior_investigation3/script.R
@@ -20,7 +20,6 @@ if (!is.numeric(paed_age_cutoff) || is.infinite(paed_age_cutoff)) {
   paed_age_cutoff <- NULL
 }
 print(paste("paed_age_cutoff is", paed_age_cutoff))
-if (!is.numeric(rw_order) || rw_order == 0) rw_order <- NULL
 
 # Revert to using planar rather than spherical geometry in `sf`
 sf::sf_use_s2(FALSE)
