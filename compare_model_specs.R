0a6135180a5083950294d1d2d559df1cef4a24c3
paddy7wb <paddy7wb@gmail.com>
2023-07-18 10:20:47 +0100

    Script for model specification comparisons

--- compare_model_specs.R
+++ compare_model_specs.R
@@ -4,6 +4,7 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(stringr)
+source("paper_poster_plots/paper/scripts/00_funs.R")
 
 #### Parameter Values ####
 
@@ -32,73 +33,19 @@ pars_df <- tidyr::crossing(
 )
 
 # uncomment to only look at models we are currently using
-pars_df <- pars_df %>%
-  filter(
-      rw_order == 0,
-      (cntry %in% no_type_iso3 & paed_age_cutoff == Inf & inc_time_tmc == FALSE) |
-      (cntry %in% vmmc_iso3 & paed_age_cutoff == 10 & inc_time_tmc == TRUE) |
-      (cntry %in% iso3 & paed_age_cutoff == Inf & inc_time_tmc == TRUE)
- )
+# pars_df <- pars_df %>%
+#   filter(
+#       rw_order == 0,
+#       (cntry %in% no_type_iso3 & paed_age_cutoff == Inf & inc_time_tmc == FALSE) |
+#       (cntry %in% vmmc_iso3 & paed_age_cutoff == 10 & inc_time_tmc == TRUE) |
+#       (cntry %in% iso3 & paed_age_cutoff == Inf & inc_time_tmc == TRUE)
+#  )
 
 # pars_df <- pars_df %>% 
 #   filter(cntry == "ZAF")
 
 
-#### Function to load orderly data ####
-
-# takes a data frame of parameters and performs an orderly search on each row.
-# By default, can also load these files (also ran parallel)
-load_orderly_data <- function(
-    # parameters fed to orderly::orderly_search
-    task,
-    parameters = NULL, # needs to be a df, not a list!
-    query = NULL, 
-    filenames = NULL, # name of specific artefact file to load, if desired
-    dirs = NULL, # optionally just provide dirs to skip orderly_search
-    load_fun = readr::read_csv, # function to load data with, if so desired
-    ncores = max(1, parallel::detectCores() - 2) 
-  ) {
-  
-  # check that either parameters & query or just dirs have been provided
-  # (could also add a query parser here for the parameters!)
-  stopifnot((!is.null(parameters) & !is.null(query)) || !is.null(dirs))
-  
-  if (is.null(dirs)) {
-    # search parameter space specified for previously run orderly tasks
-    dirs <- unlist(parallel::mclapply(seq_len(nrow(parameters)), function(i) {
-      # give progress (no longer works properly w/ mclapply rather than lapply)
-      # message(100 * (i / nrow(parameters)), "% completed") 
-      system(sprintf(
-        'echo "\n%s\n"', 
-        paste0(100 * (i / nrow(parameters)), "% completed", collapse = "")
-      ))
-      orderly::orderly_search(
-        query = query, 
-        name = task, 
-        parameters = c(parameters[i, ]) # coerces pars df to a list 
-      )
-    }, mc.cores = ncores))  
-  }
-  
-  # return NAs in parameters search, but only load files from found directories
-  dirs_return <- dirs
-  dirs <- dirs[!is.na(dirs)]
-  # return dirs if filenames unspecified
-  if (is.null(filenames)) return(list("dirs" = dirs_return))
-  files <- file.path(
-    "archive", 
-    task,
-    dirs, 
-    "artefacts/", # prob don't need this? I just structure my tasks this way
-    filenames
-  )
-  # return filenames if load_fun isn't specified
-  if (!is.null(load_fun) == FALSE) return(files)
-  return(list(
-    "dirs" = dirs_return, 
-    "output" = lapply(files, load_fun)
-  ))
-}
+#### load orderly data ####
 
 fit_stats <- load_orderly_data(
   task = "01efinal_ppc_models", 
@@ -153,17 +100,21 @@ fit_stats_join <- fit_stats_join %>%
   pivot_wider(names_from = name, values_from = value)
 
 # find missing 
-# pars_df %>% 
+# pars_df %>%
 #   anti_join(
-#     fit_stats_join, 
+#     fit_stats_join,
 #     by = c("cntry", "rw_order", "inc_time_tmc", "paed_age_cutoff")
 #   )
 
+# add VMMC/non-VMMC classification
+fit_stats_join <- fit_stats_join %>% 
+  mutate(vmmc = ifelse(cntry %in% vmmc_iso3, "VMMC", "non-VMMC")) %>% 
+  relocate(vmmc, .after = type)
 
-# best model for each country based on RMSE
-fit_stats_join %>% 
-  filter(rmse == min(rmse), .by = c(cntry, rw_order)) %>% 
-  # filter(rw_order == 0) %>% 
+# save 
+# readr::write_csv(fit_stats_join, "./fit_stats_compare_specs.csv")
+
+fit_stats_join <- fit_stats_join %>% 
   mutate(spec = paste0(
     "rw_order = ", 
     rw_order, 
@@ -171,9 +122,28 @@ fit_stats_join %>%
     paed_age_cutoff, 
     ", inc_time_tmc = ", 
     inc_time_tmc
-  )) %>% 
-  summarise(n(), .by = spec) %>% 
-  filter(grepl("rw_order = 0", spec))
+  )) 
+
+# best model for TMC for each country based on RMSE
+fit_stats_join %>% 
+  filter(rmse == min(rmse), .by = c(cntry, type, vmmc, rw_order, paed_age_cutoff, inc_time_tmc)) %>% 
+  # summarise(n(), .by = spec) %>% 
+  summarise(n = n(), .by = c(spec, type, vmmc)) %>% 
+  arrange(vmmc, type, desc(n)) %>% 
+  filter(grepl("rw_order = 0", spec)) %>% 
+  # readr::write_csv("best_mods_ar_1_rmse.csv")
+  identity()
+
+# why is non-VMMC MC missing here?!?
+
+# best model based on 0.95 CI
+fit_stats_join %>% 
+  filter(CI.0.95 == max(CI.0.95), .by = c(cntry, type, vmmc, rw_order)) %>% 
+  summarise(n = n(), .by = c(spec, type, vmmc)) %>% 
+  arrange(vmmc, type, desc(n)) %>% 
+  filter(grepl("rw_order = 0", spec)) %>% 
+  readr::write_csv("best_mods_ar_1_ci_0.95.csv")
+
 
 # Models pretty similar for VMMC countries, so essentially a modelling choice 
 # Because TMC neads to vary for KEN, makes sense to give it a time TMC effect
